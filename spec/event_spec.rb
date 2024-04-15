7a42d6c11bd27f23a03fb3415962b55f25f5ce4d
Noah Durbin <noahdurbin@gmail.com>
2024-04-15 09:43:31 -0600

    test: initialization tests for event

---
+++ spec/event_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe Event do
+  describe 'initializes' do
+    it 'exists' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event).to be_a(Event)
+    end
+
+    it 'has a name' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event.name).to eq("South Pearl Street Farmers Market")
+    end
+
+    it 'has an empty array of food trucks' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event.food_trucks).to eq([])
+    end
+end
