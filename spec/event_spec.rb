a8c39b974d18ff5a1211e6438d5a1b3b2f87358f
Noah Durbin <noahdurbin@gmail.com>
2024-04-15 10:45:35 -0600

    tests hash of total inventory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -68,5 +68,34 @@ RSpec.describe Event do
 
       expect(event.sorted_items_list).to eq(['Apple Pie (Slice)', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream'])
     end
+
+    it 'can return a hash of total inventory that reports the available inventory of all items sold at the event' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Palisade Peach Shack")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      food_truck1.stock(item1, 35)
+      food_truck2.stock(item2, 7)
+      food_truck2.stock(item3, 25)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+
+      expect(event.total_inventory).to eq(
+        {item1 => {
+          quantity: 35, 
+          food_trucks: [food_truck1]
+          }, 
+          item2 => {
+            quantity: 7, 
+            food_trucks: [food_truck2]
+          }, 
+          item3 => {
+            quantity: 25, 
+            food_trucks: [food_truck2]
+          }
+        })
+    end
   end
 end
