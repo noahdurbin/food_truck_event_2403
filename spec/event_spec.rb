a26004c3577fd345124725966e0a0215a2fcce1b
Noah Durbin <noahdurbin@gmail.com>
2024-04-15 10:11:11 -0600

    feat: event can find all trucks that sell a specific item and fixed test for this method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -43,11 +43,10 @@ RSpec.describe Event do
       food_truck1 = FoodTruck.new("Rocky Mountain Pies")
       food_truck3 = FoodTruck.new("Palisade Peach Shack")
       item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
       food_truck1.stock(item1, 35)
-      food_truck1.stock(item2, 7)
       food_truck3.stock(item1, 65)
       event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck3)
 
       expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
     end
