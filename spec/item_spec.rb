c76f423c9331e8b1f1ae7e4decaa44320a455f84
Noah Durbin <noahdurbin@gmail.com>
2024-04-15 09:17:16 -0600

    feat: item initializes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
-RSpec describe Item do
-  it 'exists' do
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    expect(item1).to be_a(Item)
+RSpec.describe Item do
+  describe 'initializes' do
+    it 'exists' do
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      expect(item1).to be_a(Item)
+    end
   end
 end
