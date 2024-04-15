ea14c06e8e5bfc060df6467c559ecab0deea63a8
Noah Durbin <noahdurbin@gmail.com>
2024-04-15 09:35:16 -0600

    feat: food truck stocking methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,9 +1,21 @@
 class FoodTruck
 
   attr_reader :name, :inventory
-  
+
   def initialize(name)
     @name = name
     @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory[item].nil?
+      0
+    else
+      @inventory[item]
+    end
+  end
+
+  def stock(item, amount)
+    @inventory[item] = amount
   end
 end
