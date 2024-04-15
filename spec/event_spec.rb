673ed388f91a1bd24d0359e7a99f2cf6318767d4
Noah Durbin <noahdurbin@gmail.com>
2024-04-15 09:56:27 -0600

    test: tests for food truck event methods

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -20,4 +20,36 @@ RSpec.describe Event do
       expect(event.food_trucks).to eq([])
     end
   end
+
+  describe 'food truck methods' do
+    it 'can add food trucks to the food truck array' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      event.add_food_truck(food_truck1)
+
+      expect(event.food_trucks).to eq([food_truck1])
+    end
+
+    it 'can return a list of the names of food trucks at event' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      event.add_food_truck(food_truck1)
+
+      expect(event.food_truck_names).to eq(["Rocky Mountain Pies"])
+    end
+
+    it 'can return a list of food trucks that sell a specific item' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck3.stock(item1, 65)
+      event.add_food_truck(food_truck1)
+
+      expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
+    end
+  end
 end
